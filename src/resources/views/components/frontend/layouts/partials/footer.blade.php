eb8a498d21a8405133a415c3739dc7b91a3c2038
topu <topukhan735@gmail.com>
2024-01-08 20:29:40 +0600

    footer layout modify

--- src/resources/views/components/frontend/layouts/partials/footer.blade.php
+++ src/resources/views/components/frontend/layouts/partials/footer.blade.php
@@ -1,172 +1,177 @@
 <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
     <!--begin::Container-->
-    <div
-        class="container-xxl d-flex flex-column flex-md-row align-items-center justify-content-between">
+    <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Row-->
-        <div class="row py-10 py-lg-20">
+        <div class="row g-8">
             <!--begin::Col-->
-            <div class="col-lg-6 pe-lg-16 mb-10 mb-lg-0">
-                <div class="header-logo me-5 me-md-10 flex-grow-1 flex-lg-grow-0">
-                    <a href="{{route('home')}}">
-                        <img alt="Logo" src="{{ asset('ui/frontend/assets')}}/media/logos/logo-sticky.png"
+            <div class="col-lg-5 col-md-6 pe-8">
+                <div class="footer-logo me-5 me-md-10 flex-grow-1 flex-lg-grow-0">
+                    <a href="{{ route('home') }}">
+                        <img alt="Logo" src="{{ asset('ui/frontend/assets') }}/media/logos/logo-sticky.png"
                             class="logo-sticky h-40px" />
                     </a>
                 </div>
-                <div class="rounded">
-                    <div class="a2i-no-repeat pt-2"
-                        style="height: 180px; width: auto; background-image:url('{{ asset('ui/frontend/assets')}}/media/logos/a2i-all.png'); background-position: left top; background-size: 100% auto;">
-                    </div>
+                <div class="mt-4 w-75">
+                    <img class="img-fluid" src="{{ asset('ui/frontend/assets') }}/media/logos/a2i-all.png"
+                        alt="a2i partners">
                 </div>
             </div>
             <!--end::Col-->
             <!--begin::Col-->
-            <div class="col-lg-6 ps-lg-16">
-                <!--begin::Navs-->
-                <div class="d-flex justify-content-center">
-                    <div class="row">
-                        <div class="col-3">
-                            <div class="d-flex fw-semibold flex-column">
-                                <!--begin::Subtitle-->
-                                <h4 class="fw-bold text-gray-500 mb-6">Company</h4>
-                                <!--end::Subtitle-->
+            <div class="col-lg-2 col-md-6">
+                <!--begin::Subtitle-->
+                <h4 class="fw-bold text-gray-500 mb-6">Company</h4>
+                <!--end::Subtitle-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="#" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">About</a>
+                </div>
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="#" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our
+                        Services</a>
+                </div>
+
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="#" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our Missions</a>
+                </div>
+
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="#" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our Sites</a>
+                </div>
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="#" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Portal Login</a>
+                </div>
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-3">
+                    <a href="https://www.sentineltechno.com/" target="_blank"
+                        class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Support Login</a>
+                </div>
+                <!--end::Link-->
+            </div>
+            <!--end::Col-->
+            <!--begin::Col-->
+            <div class="col-lg-2 col-md-6">
+                <!--begin::Subtitle-->
+                <h4 class="fw-bold text-gray-500 mb-6">Quick Links</h4>
+                <!--end::Subtitle-->
+                <!--begin::Link-->
+                <div class="mb-4">
+                    <a href="https://cabinet.gov.bd/"
+                        class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Cabinet
+                        Division</a>
+                </div>
+
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-4">
+                    <a href="https://ictd.gov.bd/" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">ICT
+                        Division</a>
+                </div>
+
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-4">
+                    <a href="https://www.undp.org/bangladesh"
+                        class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">United
+                        Nations Development <br> Programme (UNDP), Bangladesh</a>
+                </div>
+
+                <!--end::Link-->
+                <!--begin::Link-->
+                <div class="mb-4">
+                    <a href="https://a2i.gov.bd/" class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">A2i
+                        Program</a>
+                </div>
+                <!--end::Link-->
+            </div>
+            <!--end::Col-->
+            <!--begin::Col-->
+            <div class="col-lg-3 col-md-6">
+                <div class="d-flex fw-semibold flex-column">
+                    <!--begin::Subtitle-->
+                    <h4 class="fw-bold text-gray-500 mb-6">Contact</h4>
+                    <!--end::Subtitle-->
+                    <div>
+                        <i class="ki-duotone ki-pointers">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                        </i>
+                        <span class="fs-5 text-gray-500">Aspire to Innovate (a2i) Programme
+                            E-14/X,
+                            ICT Tower Agargaon, Sher-e-Bangla Nagar, Dhaka-1207, Bangladesh
+                        </span>
+                    </div>
+                    <div class="pt-4">
+                        <i class="ki-duotone ki-call">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                            <span class="path3"></span>
+                            <span class="path4"></span>
+                            <span class="path5"></span>
+                            <span class="path6"></span>
+                            <span class="path7"></span>
+                            <span class="path8"></span>
+                        </i>
+                        <span class="fs-5 text-gray-500">88 02 55006931-34</span>
+                    </div>
+                    <div class="pt-4">
+                        <i class="ki-duotone ki-sms">
+                            <span class="path1"></span>
+                            <span class="path2"></span>
+                        </i>
+                        <span class="fs-5 text-gray-500">info@a2i.gov.bd</span>
+                    </div>
+                    <div class="pt-6">
+                        <div class="row">
+                            <div class="text-center col-2">
                                 <!--begin::Link-->
-                                <a href="#"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">About</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="#"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our
-                                    Services</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="#"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our
-                                    Missions</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="#"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Our
-                                    Sites</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="#"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Portal
-                                    Login</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="https://www.sentineltechno.com/" target="_blank"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Support
-                                    Login</a>
-                                <!--end::Link-->
+                                <a href="#" class="mb-4">
+                                    <img src="{{ asset('ui/frontend/assets') }}/media/svg/brand-logos/facebook-4.svg"
+                                        class="h-20px me-2" alt="" />
+                                </a>
                             </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="d-flex fw-semibold flex-column">
-                                <!--begin::Subtitle-->
-                                <h4 class="fw-bold text-gray-500 mb-6">Quick Links</h4>
-                                <!--end::Subtitle-->
+                            <!--end::Link-->
+                            <div class="text-center col-2">
                                 <!--begin::Link-->
-                                <a href="https://cabinet.gov.bd/"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">Cabinet
-                                    Division</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="https://ictd.gov.bd/"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">ICT
-                                    Division</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="https://www.undp.org/bangladesh"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">United
-                                    Nations Development <br> Programme (UNDP), Bangladesh</a>
-                                <!--end::Link-->
-                                <!--begin::Link-->
-                                <a href="https://a2i.gov.bd/"
-                                    class="text-gray-800 opacity-50 text-hover-primary fs-5 mb-3">A2i
-                                    Program</a>
-                                <!--end::Link-->
+                                <a href="#" class="mb-4">
+                                    <img src="{{ asset('ui/frontend/assets') }}/media/svg/brand-logos/twitter.svg"
+                                        class="h-20px me-2" alt="" />
+                                </a>
                             </div>
-                        </div>
-                        <div class="col-5">
-                            <div class="d-flex fw-semibold flex-column">
-                                <!--begin::Subtitle-->
-                                <h4 class="fw-bold text-gray-500 mb-6">Contact</h4>
-                                <!--end::Subtitle-->
-                                <div>
-                                    <i class="ki-duotone ki-pointers">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                    </i>
-                                    <span class="fs-5 text-gray-500">Aspire to Innovate (a2i) Programme
-                                        E-14/X,
-                                        ICT Tower Agargaon, Sher-e-Bangla Nagar, Dhaka-1207, Bangladesh
-                                    </span>
-                                </div>
-                                <div class="pt-4">
-                                    <i class="ki-duotone ki-call">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                        <span class="path4"></span>
-                                        <span class="path5"></span>
-                                        <span class="path6"></span>
-                                        <span class="path7"></span>
-                                        <span class="path8"></span>
-                                    </i>
-                                    <span class="fs-5 text-gray-500">88 02 55006931-34</span>
-                                </div>
-                                <div class="pt-4">
-                                    <i class="ki-duotone ki-sms">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                    <span class="fs-5 text-gray-500">info@a2i.gov.bd</span>
-                                </div>
-                                <div class="pt-6">
-                                    <div class="row">
-                                        <div class="text-center col-2">
-                                            <!--begin::Link-->
-                                            <a href="#" class="mb-4">
-                                                <img src="{{ asset('ui/frontend/assets')}}/media/svg/brand-logos/facebook-4.svg"
-                                                    class="h-20px me-2" alt="" />
-                                            </a>
-                                        </div>
-                                        <!--end::Link-->
-                                        <div class="text-center col-2">
-                                            <!--begin::Link-->
-                                            <a href="#" class="mb-4">
-                                                <img src="{{ asset('ui/frontend/assets')}}/media/svg/brand-logos/twitter.svg"
-                                                    class="h-20px me-2" alt="" />
-                                            </a>
-                                        </div>
-                                        <!--end::Link-->
-                                        <div class="text-center col-2">
-                                            <!--begin::Link-->
-                                            <a href="#" class="mb-4">
-                                                <img src="{{ asset('ui/frontend/assets')}}/media/svg/brand-logos/instagram-2-1.svg"
-                                                    class="h-20px me-2" alt="" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--end::Link-->
+                            <!--end::Link-->
+                            <div class="text-center col-2">
+                                <!--begin::Link-->
+                                <a href="#" class="mb-4">
+                                    <img src="{{ asset('ui/frontend/assets') }}/media/svg/brand-logos/instagram-2-1.svg"
+                                        class="h-20px me-2" alt="" />
+                                </a>
                             </div>
                         </div>
                     </div>
+                    <!--end::Link-->
                 </div>
-                <!--end::Navs-->
             </div>
             <!--end::Col-->
+
             <!--begin::Copyright-->
             <div class="text-gray-900 order-2 order-md-1">
                 <div style="padding: 9.5px 0px;">
-                    <span class="text-muted fw-semibold me-1">2023 &copy;</span>
-                    <a href="https://letterbuilder.com" target="_blank"
-                        class="text-gray-800 text-hover-primary">Letter Builder</a>
+                    <span class="text-muted fw-semibold me-1">2024 &copy;</span>
+                    <a href="https://letterbuilder.com" target="_blank" class="text-gray-800 text-hover-primary">Letter
+                        Builder</a>
                     <span class="text-muted fw-semibold me-1"><i>Technology Partner</i></span>
                     <a href="https://www.sentineltechno.com">
-                        <img alt="Logo" src="{{ asset('ui/frontend/assets')}}/media/logos/sentinel.png"
+                        <img alt="Logo" src="{{ asset('ui/frontend/assets') }}/media/logos/sentinel.png"
                             class="logo-default h-30px" />
                     </a>
                 </div>
